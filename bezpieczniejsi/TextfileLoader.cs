036cc0edae9e14f23b457348042f01cdbb154b0b
piatkosia <piatkosia.apt@interia.pl>
2016-03-08 22:07:22 +0100

    Konsolki już nie używam;)

---
+++ bezpieczniejsi/TextfileLoader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bezpieczniejsi
+{
+    class TextfileLoader: LoadingProvider
+    {
+        public RiskBank loadRisks(object source)
+        {
+            if (source is string)
+            {
+                Encoding code;
+                RiskBank bank = new RiskBank();
+                bank.Title = Path.GetFileNameWithoutExtension((string) source);
+                string line;
+                System.IO.StreamReader file =
+                   new System.IO.StreamReader((string)source);
+                while ((line = file.ReadLine()) != null)
+                {
+                    bank.risks.Add(line);
+                }
+                return bank;
+            }
+            else return null;
+        }
+
+    }
+}
